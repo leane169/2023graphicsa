c0cc67ae6c6d77e7591f0ddf135aea01285c22ad
leane169 <710334@cyvs.tyc.edu.tw>
2023-03-22 12:00:03 +0800

    這次更新week06

---
+++ week06-1_TRT_robot2/main.cpp
@@ -0,0 +1,42 @@
+#include <GL/glut.h>
+
+float angle = 0; ///step02-1 �ŧiglobal�����ܼ� angle
+
+void display()
+
+{
+
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ); ///�M�I��
+
+    glutSolidSphere(0.02,30,30);
+    glPushMatrix();
+        glRotatef(angle,0,0,1);
+        glTranslatef(0.46,-0.05,0);
+        glutSolidTeapot(0.3);
+    glPopMatrix();
+
+    glutSwapBuffers();
+
+    angle++; ///step02-1 �⨤��++
+
+}
+
+int main(int argc, char* argv[] )
+
+{
+
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
+
+    glutCreateWindow("week04");
+
+
+
+    glutDisplayFunc(display);
+
+    glutIdleFunc(display); ///step02-1 ����idle��,�N���e�e��
+
+    glutMainLoop();
+
+}
